1e4cb0735b4cd76753162204fd01612fe15eb6a2
Xu Xue <xuuexu@gmail.com>
2020-08-13 19:02:46 +0800

    Lists 50%

--- Lists.v
+++ Lists.v
@@ -62,3 +62,58 @@ Definition mylist := cons 1 (cons 2 (cons 3 nil)).
 Notation "x :: l" := (cons x l) (at level 60, right associativity).
 Notation "[ ]" := nil.
 Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
+Fixpoint repeat (n count : nat) : natlist :=
+  match count with
+  | O => nil
+  | S count' => n :: (repeat n count')
+  end.
+
+Fixpoint length (l : natlist) : nat :=
+  match l with
+  | nil => O
+  | h :: t => S (length t)
+  end.
+
+Fixpoint app (l1 l2 : natlist) : natlist :=
+  match l1 with
+  | nil => l2
+  | h :: t => h :: (app t l2)
+  end.
+
+Notation "x ++ y" := (app x y) (right associativity, at level 60).
+Example test_app1: [1;2;3] ++ [4;5] = [1;2;3;4;5].
+Proof. reflexivity. Qed.
+Example test_app2: nil ++ [4;5] = [4;5].
+Proof. reflexivity. Qed.
+Example test_app3: [1;2;3] ++ nil = [1;2;3].
+Proof. reflexivity. Qed.
+
+Definition hd (default : nat) (l : natlist) : nat :=
+  match l with
+  | nil => default
+  | h :: t => h
+  end.
+
+Definition tl (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => t
+  end.
+Example test_hd1: hd 0 [1;2;3] = 1.
+Proof. reflexivity. Qed.
+Example test_hd2: hd 0 [] = 0.
+Proof. reflexivity. Qed.
+Example test_tl: tl [1;2;3] = [2;3].
+Proof. reflexivity. Qed.
+
+Fixpoint nonzeros (l : natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => match h with
+              | O => nonzeros t
+              | _ => h :: nonzeros t
+              end
+  end.
+
+Compute (nonzeros (cons 0 (cons 1 (cons 0 (cons 1 nil))))).
