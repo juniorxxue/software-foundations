6f85d5b054bd7cfe05ed84028795bef33083368d
Shawn Xu <xuuexu@gmail.com>
2020-09-08 23:20:55 +0800

    begin plf

---
+++ plf/Equiv.v
@@ -0,0 +1,12 @@
+Set Warnings "-notation-overridden,-parsing".
+From PLF Require Import Maps.
+From Coq Require Import Bool.Bool.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.PeanoNat. Import Nat.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import Lia.
+From Coq Require Import Lists.List.
+From Coq Require Import Logic.FunctionalExtensionality.
+Import ListNotations.
+From PLF Require Export Imp.
