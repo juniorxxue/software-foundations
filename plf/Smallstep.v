11c70e9ef1fdf25bbbe40e82021cf30bd4ea94dc
Shawn Xu <xuuexu@gmail.com>
2020-09-18 17:25:58 +0800

    types main contents

--- plf/Smallstep.v
+++ plf/Smallstep.v
@@ -293,5 +293,15 @@ Proof.
     now apply step_eval with y.
 Qed.
 
+Ltac solve_by_inverts n :=
+  match goal with | H : ?T |- _ =>
+  match type of T with Prop =>
+    solve [
+      inversion H;
+      match n with S (S (?n')) => subst; solve_by_inverts (S n') end ]
+  end end.
 
+
+Ltac solve_by_invert :=
+  solve_by_inverts 1.
 
--- plf/Types.v
+++ plf/Types.v
@@ -26,6 +26,8 @@ Definition value (t : tm) := bvalue t \/ nvalue t.
 Hint Constructors bvalue nvalue : core.
 Hint Unfold value : core.
 
+Reserved Notation "t '-->' t'" (at level 40).
+
 Inductive step : tm -> tm -> Prop :=
   | ST_TestTru : forall t1 t2,
       (test tru t1 t2) --> t1
@@ -52,7 +54,11 @@ Inductive step : tm -> tm -> Prop :=
       (iszro (scc v)) --> fls
   | ST_Iszro : forall t1 t1',
       t1 --> t1' ->
-      (iszro t1) --> (iszro t1').
+      (iszro t1) --> (iszro t1')
+
+where "t '-->' t'" := (step t t').
+
+Hint Constructors step : core.
 
 Notation step_normal_form := (normal_form step).
 
@@ -60,6 +66,135 @@ Definition stuck (t : tm) : Prop :=
   step_normal_form t /\ ~ value t.
 
 Hint Unfold stuck : core.
-Example some_term_is_stuck :
-  exists t, stuck t.
+
+Inductive ty : Type :=
+| Bool : ty
+| Nat : ty.
+
+Reserved Notation "'|-' t '\in' T" (at level 40).
+
+Inductive has_type : tm -> ty -> Prop :=
+  | T_Tru :
+       |- tru \in Bool
+  | T_Fls :
+       |- fls \in Bool
+  | T_Test : forall t1 t2 t3 T,
+       |- t1 \in Bool ->
+       |- t2 \in T ->
+       |- t3 \in T ->
+       |- test t1 t2 t3 \in T
+  | T_Zro :
+       |- zro \in Nat
+  | T_Scc : forall t1,
+       |- t1 \in Nat ->
+       |- scc t1 \in Nat
+  | T_Prd : forall t1,
+       |- t1 \in Nat ->
+       |- prd t1 \in Nat
+  | T_Iszro : forall t1,
+       |- t1 \in Nat ->
+       |- iszro t1 \in Bool
+where "'|-' t '\in' T" := (has_type t T).
+
+Hint Constructors has_type : core.
+
+Example has_type_1 :
+  |- test fls zro (scc zro) \in Nat.
 Proof.
+  apply T_Test.
+  - apply T_Fls.
+  - apply T_Zro.
+  - apply T_Scc. apply T_Zro.
+Qed.
+
+Example scc_hastype_nat__hastype_nat : forall t,
+  |- scc t \in Nat ->
+  |- t \in Nat.
+Proof.
+  intros.
+  inversion H.
+  apply H1.
+Qed.
+
+Lemma bool_canonical : forall t,
+    |- t \in Bool -> value t -> bvalue t.
+Proof.
+  intros t Ht [Hb | Hn].
+  - assumption.
+  - destruct Hn as [ | Hs].
+    + inversion Ht.
+    + inversion Ht.
+Qed.
+
+Lemma nat_canonical : forall t,
+    |- t \in Nat -> value t -> nvalue t.
+Proof.
+  intros t Ht [Hb | Hn].
+  - inversion Hb.
+    + subst. inversion Ht.
+    + subst. inversion Ht.
+  - assumption.
+Qed.
+(* Use type error to invert it *)
+
+Theorem progress : forall t T,
+  |- t \in T ->
+          value t \/ exists t', t --> t'.
+Proof.
+  intros t T HT.
+  induction HT; auto.
+  (* The cases that were obviously values, like T_Tru and
+     T_Fls, were eliminated immediately by auto *)
+  - (* T_Test *)
+    right. destruct IHHT1.
+    + (* t1 is a value *)
+    apply (bool_canonical t1 HT1) in H.
+    destruct H.
+      * exists t2. auto.
+      * exists t3. auto.
+    + (* t1 can take a step *)
+      destruct H as [t1' H1].
+      exists (test t1' t2 t3). auto.
+  - (* T_Succ *)
+    destruct IHHT.
+    + left.
+Admitted.
+
+(* Type Preservation *)
+Theorem preservation : forall t t' T,
+    |- t \in T ->
+            t --> t' ->
+            |- t' \in T.
+Proof.
+  intros t t' T HT HE.
+  generalize dependent t'.
+  induction HT;
+         (* every case needs to introduce a couple of things *)
+         intros t' HE;
+         (* and we can deal with several impossible
+            cases all at once *)
+         try solve_by_invert.
+    - (* T_Test *) inversion HE; subst; clear HE.
+      + (* ST_TESTTru *) assumption.
+      + (* ST_TestFls *) assumption.
+      + (* ST_Test *) apply T_Test; try assumption.
+        apply IHHT1; assumption.
+    (* FILL IN HERE *) Admitted.
+Definition multistep := (multi step).
+Notation "t1 '-->*' t2" := (multistep t1 t2) (at level 40).
+
+Check step.
+
+Check multi.
+
+Corollary soundness : forall t t' T,
+  |- t \in T ->
+  t -->* t' ->
+  ~(stuck t').
+Proof.
+  intros t t' T HT P. induction P; intros [R S].
+  - apply progress in HT. destruct HT; auto.
+  - apply IHP.
+    + apply preservation with (t := x); auto.
+    + unfold stuck. split; auto.
+Qed.
