f54eeec24c6ce89275c458fea12823818b6a10b0
Xu Xue <juniorxxue@gmail.com>
2021-02-22 20:09:59 +0800

    reread Imp chapter

--- lf/Imp.v
+++ lf/Imp.v
@@ -9,14 +9,21 @@ From Coq Require Import Strings.String.
 Import ListNotations.
 From LF Require Import Maps.
 
-Module AExp.
-  Inductive aexp : Type :=
-  | ANum (n : nat)
-  | APlus (a1 a2 : aexp)
-  | AMinus (a1 a2 : aexp)
-  | AMult (a1 a2 : aexp).
+Definition state := total_map nat.
 
-  Inductive bexp : Type :=
+Inductive aexp : Type :=
+| ANum (n : nat)
+| AId (x : string)
+| APlus (a1 a2 : aexp)
+| AMinus (a1 a2 : aexp)
+| AMult (a1 a2 : aexp).
+
+Definition X : string := "X".
+Definition Y : string := "Y".
+Definition Z : string := "Z".
+Definition W : string := "W".
+
+Inductive bexp : Type :=
   | BTrue
   | BFalse
   | BEq (a1 a2 : aexp)
@@ -24,51 +31,110 @@ Module AExp.
   | BNot (b : bexp)
   | BAnd (b1 b2 : bexp).
 
-  Fixpoint aeval (a : aexp) : nat :=
-    match a with
-    | ANum n => n
-    | APlus a1 a2 => (aeval a1) + (aeval a2)
-    | AMinus a1 a2 => (aeval a1) - (aeval a2)
-    | AMult a1 a2 => (aeval a1) * (aeval a2)
-    end.
+Coercion AId : string >-> aexp.
+Coercion ANum : nat >-> aexp.
 
-  Fixpoint beval (b : bexp) : bool :=
-    match b with
-    | BTrue => true
-    | BFalse => false
-    | BEq a1 a2 => (aeval a1) =? (aeval a2)
-    | BLe a1 a2 => (aeval a1) <=? (aeval a2)
-    | BNot b1 => negb (beval b1)
-    | BAnd b1 b2 => andb (beval b1) (beval b2)
-    end.
+Declare Custom Entry com.
+Declare Scope com_scope.
+Notation "<{ e }>" := e (at level 0, e custom com at level 99) : com_scope.
+Notation "( x )" := x (in custom com, x at level 99) : com_scope.
+Notation "x" := x (in custom com at level 0, x constr at level 0) : com_scope.
+Notation "f x .. y" := (.. (f x) .. y)
+                  (in custom com at level 0, only parsing,
+                  f constr at level 0, x constr at level 9,
+                  y constr at level 9) : com_scope.
+Notation "x + y" := (APlus x y) (in custom com at level 50, left associativity).
+Notation "x - y" := (AMinus x y) (in custom com at level 50, left associativity).
+Notation "x * y" := (AMult x y) (in custom com at level 40, left associativity).
+Notation "'true'"  := true (at level 1).
+Notation "'true'"  := BTrue (in custom com at level 0).
+Notation "'false'"  := false (at level 1).
+Notation "'false'"  := BFalse (in custom com at level 0).
+Notation "x <= y" := (BLe x y) (in custom com at level 70, no associativity).
+Notation "x = y"  := (BEq x y) (in custom com at level 70, no associativity).
+Notation "x && y" := (BAnd x y) (in custom com at level 80, left associativity).
+Notation "'~' b"  := (BNot b) (in custom com at level 75, right associativity).
 
-  Fixpoint optimize_0plus (a : aexp) : aexp :=
-    match a with
-    | ANum n =>  ANum n
-    | APlus (ANum 0) e2 => optimize_0plus e2
-    | APlus e1 e2 => APlus (optimize_0plus e1) (optimize_0plus e2)
-    | AMinus e1 e2 => AMinus (optimize_0plus e1) (optimize_0plus e2)
-    | AMult e1 e2 => AMult (optimize_0plus e1) (optimize_0plus e2)
-    end.
-  Example test_optimize_0plus:
-    optimize_0plus (APlus (ANum 2)
-                          (APlus (ANum 0)
-                                 (APlus (ANum 0) (ANum 1))))
-    = APlus (ANum 2) (ANum 1).
-  Proof. reflexivity. Qed.
+Open Scope com_scope.
 
-  Theorem silly1 : forall ae, aeval ae = aeval ae.
-  Proof.
-    try reflexivity. Qed.
+Fixpoint aeval (st : state) (a : aexp) : nat :=
+  match a with
+  | ANum n => n
+  | AId x => st x
+  | <{a1 + a2}> => (aeval st a1) + (aeval st a2)
+  | <{a1 - a2}> => (aeval st a1) - (aeval st a2)
+  | <{a1 * a2}> => (aeval st a1) * (aeval st a2)
+  end.
 
-  Lemma foo : forall n, 0 <=? n = true.
-  Proof.
-    intros.
-    destruct n; simpl; reflexivity.
-  Qed.
+Fixpoint beval (st : state) (b : bexp) : bool :=
+  match b with
+  | <{true}>      => true
+  | <{false}>     => false
+  | <{a1 = a2}>   => (aeval st a1) =? (aeval st a2)
+  | <{a1 <= a2}>  => (aeval st a1) <=? (aeval st a2)
+  | <{~ b1}>      => negb (beval st b1)
+  | <{b1 && b2}>  => andb (beval st b1) (beval st b2)
+  end.
 
-  Theorem In10 : In 10 [1;2;3;4;5;6;7;8;9;10].
-  Proof.
-    repeat (try (left; reflexivity); right).
-  Qed.
 
+Definition empty_st := (_ !-> 0).
+Notation "x '!->' v" := (t_update empty_st x v) (at level 100).
+
+
+Inductive com : Type :=
+| CSkip
+| CAss (x : string) (a : aexp)
+| CSeq (c1 c2 : com)
+| CIf (b : bexp) (c1 c2 : com)
+| CWhile (b : bexp) (c : com).
+
+Notation "'skip'"  :=
+         CSkip (in custom com at level 0) : com_scope.
+Notation "x := y"  :=
+         (CAss x y)
+            (in custom com at level 0, x constr at level 0,
+             y at level 85, no associativity) : com_scope.
+Notation "x ; y" :=
+         (CSeq x y)
+           (in custom com at level 90, right associativity) : com_scope.
+Notation "'if' x 'then' y 'else' z 'end'" :=
+         (CIf x y z)
+           (in custom com at level 89, x at level 99,
+            y at level 99, z at level 99) : com_scope.
+Notation "'while' x 'do' y 'end'" :=
+         (CWhile x y)
+            (in custom com at level 89, x at level 99, y at level 99) : com_scope.
+
+Reserved Notation
+         "st '=[' c ']=>' st'"
+         (at level 40, c custom com at level 99,
+          st constr, st' constr at next level).
+
+Inductive ceval : com -> state -> state -> Prop :=
+  | E_Skip : forall st,
+      st =[ skip ]=> st
+  | E_Ass  : forall st a n x,
+      aeval st a = n ->
+      st =[ x := a ]=> (x !-> n ; st)
+  | E_Seq : forall c1 c2 st st' st'',
+      st  =[ c1 ]=> st'  ->
+      st' =[ c2 ]=> st'' ->
+      st  =[ c1 ; c2 ]=> st''
+  | E_IfTrue : forall st st' b c1 c2,
+      beval st b = true ->
+      st =[ c1 ]=> st' ->
+      st =[ if b then c1 else c2 end]=> st'
+  | E_IfFalse : forall st st' b c1 c2,
+      beval st b = false ->
+      st =[ c2 ]=> st' ->
+      st =[ if b then c1 else c2 end]=> st'
+  | E_WhileFalse : forall b st c,
+      beval st b = false ->
+      st =[ while b do c end ]=> st
+  | E_WhileTrue : forall st st' st'' b c,
+      beval st b = true ->
+      st  =[ c ]=> st' ->
+      st' =[ while b do c end ]=> st'' ->
+      st  =[ while b do c end ]=> st''
+
+  where "st =[ c ]=> st'" := (ceval c st st').
