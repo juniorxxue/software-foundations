4a52d245ba5b7f00e1a1fad33b9d17e6414a49e4
Xu Xue <juniorxxue@gmail.com>
2021-02-24 11:35:50 +0800

    Hoare 1

---
+++ plf/Hoare.v
@@ -0,0 +1,178 @@
+Set Warnings "-notation-overridden,-parsing".
+From PLF Require Import Maps.
+From Coq Require Import Bool.Bool.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import Arith.PeanoNat. Import Nat.
+From Coq Require Import Lia.
+From PLF Require Export Imp.
+
+Definition Assertion := state -> Prop.
+
+
+Definition assert_implies (P Q : Assertion) : Prop :=
+  forall st, P st -> Q st.
+
+Declare Scope hore_spec_scope.
+Notation "P ->> Q" := (assert_implies P Q)
+                        (at level 80) : hoare_spec_scope.
+
+Open Scope hoare_spec_scope.
+
+Notation "P <<->> Q" :=
+  (P ->> Q /\ Q ->> P) (at level 80) : hoare_spec_scope.
+
+Definition Aexp : Type := state -> nat.
+
+Definition assert_of_Prop (P : Prop) : Assertion := fun _ => P.
+Definition Aexp_of_nat (n : nat) : Aexp := fun _ => n.
+
+Definition Aexp_of_aexp (a : aexp) : Aexp := fun st => aeval st a.
+
+Coercion assert_of_Prop : Sortclass >-> Assertion.
+Coercion Aexp_of_nat : nat >-> Aexp.
+Coercion Aexp_of_aexp : aexp >-> Aexp.
+
+Arguments assert_of_Prop /.
+Arguments Aexp_of_nat /.
+Arguments Aexp_of_aexp /.
+
+Declare Scope assertion_scope.
+Bind Scope assertion_scope with Assertion.
+Bind Scope assertion_scope with Aexp.
+Delimit Scope assertion_scope with assertion.
+
+Notation assert P := (P%assertion : Assertion).
+Notation mkAexp a := (a%assertion : Aexp).
+
+Notation "~ P" := (fun st => ~ assert P st) : assertion_scope.
+Notation "P /\ Q" := (fun st => assert P st /\ assert Q st) : assertion_scope.
+Notation "P \/ Q" := (fun st => assert P st \/ assert Q st) : assertion_scope.
+Notation "P -> Q" := (fun st => assert P st ->  assert Q st) : assertion_scope.
+Notation "P <-> Q" := (fun st => assert P st <->  assert Q st) : assertion_scope.
+Notation "a = b" := (fun st => mkAexp a st = mkAexp b st) : assertion_scope.
+Notation "a <> b" := (fun st => mkAexp a st <> mkAexp b st) : assertion_scope.
+Notation "a <= b" := (fun st => mkAexp a st <= mkAexp b st) : assertion_scope.
+Notation "a < b" := (fun st => mkAexp a st < mkAexp b st) : assertion_scope.
+Notation "a >= b" := (fun st => mkAexp a st >= mkAexp b st) : assertion_scope.
+Notation "a > b" := (fun st => mkAexp a st > mkAexp b st) : assertion_scope.
+Notation "a + b" := (fun st => mkAexp a st + mkAexp b st) : assertion_scope.
+Notation "a - b" := (fun st => mkAexp a st - mkAexp b st) : assertion_scope.
+Notation "a * b" := (fun st => mkAexp a st * mkAexp b st) : assertion_scope.
+
+Definition ap {X} (f : nat -> X) (x : Aexp) :=
+  fun st => f (x st).
+
+Definition ap2 {X} (f : nat -> nat -> X) (x : Aexp) (y : Aexp) (st : state) :=
+  f (x st) (y st).
+
+
+Definition assn1 : Assertion := X <= Y.
+
+Definition hoare_triple
+           (P : Assertion) (c : com) (Q : Assertion) : Prop :=
+  forall st st',
+     st =[ c ]=> st'  ->
+     P st  ->
+     Q st'.
+
+Notation "{{ P }}  c  {{ Q }}" :=
+  (hoare_triple P c Q) (at level 90, c custom com at level 99)
+  : hoare_spec_scope.
+
+Theorem hoare_post_true :
+  forall (P Q : Assertion) c,
+  (forall st, Q st) -> {{P}} c {{Q}}.
+Proof.
+  intros.
+  unfold hoare_triple.
+  intros. apply H.
+Qed.
+
+Theorem hoare_pre_false : forall (P Q : Assertion) c,
+  (forall st, ~ (P st)) ->
+  {{P}} c {{Q}}.
+Proof.
+  intros. unfold hoare_triple. intros.
+  apply H in H1. inversion H1.
+Qed.
+
+(* The goal of Hoare logic is
+to provide a compositional method
+for proving the validity of specific Hoare triples.*)
+
+Definition assn_sub X a (P:Assertion) : Assertion :=
+  fun (st : state) =>
+    P (X !-> aeval st a ; st).
+
+Notation "P [ X |-> a ]" := (assn_sub X a P)
+  (at level 10, X at next level, a custom com).
+
+Theorem hoare_asgn : forall Q X a,
+  {{Q [X |-> a]}} X := a {{Q}}.
+Proof.
+  unfold hoare_triple.
+  intros Q X a st st' HE HQ.
+  inversion HE. subst.
+  unfold assn_sub in HQ. assumption.
+Qed.
+
+Theorem hoare_consequence_pre : forall (P P' Q : Assertion) c,
+  {{P'}} c {{Q}} ->
+  P ->> P' ->
+  {{P}} c {{Q}}.
+Proof.
+  unfold hoare_triple, "->>".
+  intros P P' Q c Hhoare Himp st st' Heval Hpre.
+  apply Hhoare with (st := st).
+  - assumption.
+  - apply Himp. assumption.
+Qed.
+
+Theorem hoare_consequence_post : forall (P Q Q' : Assertion) c,
+  {{P}} c {{Q'}} ->
+  Q' ->> Q ->
+  {{P}} c {{Q}}.
+Proof.
+  unfold hoare_triple, "->>".
+  intros P Q Q' c Hhoare Himp st st' Heval Hpre.
+  apply Himp.
+  apply Hhoare with (st := st).
+  - assumption.
+  - assumption.
+Qed.
+
+Theorem hoare_consequence : forall (P P' Q Q' : Assertion) c,
+  {{P'}} c {{Q'}} ->
+  P ->> P' ->
+  Q' ->> Q ->
+  {{P}} c {{Q}}.
+Proof.
+  intros P P' Q Q' c Htriple Hpre Hpost.
+  apply hoare_consequence_pre with (P' := P').
+  - apply hoare_consequence_post with (Q' := Q').
+    + assumption.
+    + assumption.
+  - assumption.
+Qed.
+
+Hint Unfold assert_implies hoare_triple assn_sub t_update : core.
+Hint Unfold assert_of_Prop Aexp_of_nat Aexp_of_aexp : core.
+
+Theorem hoare_skip : forall P,
+     {{P}} skip {{P}}.
+Proof.
+  intros P st st' H HP. inversion H; subst. assumption.
+Qed.
+
+Theorem hoare_seq : forall P Q R c1 c2,
+     {{Q}} c2 {{R}} ->
+     {{P}} c1 {{Q}} ->
+     {{P}} c1; c2 {{R}}.
+Proof.
+  unfold hoare_triple.
+  intros P Q R c1 c2 H1 H2 st st' H12 Pre.
+  inversion H12; subst.
+  eapply H1; try (repeat eassumption).
+  eapply H2; try (repeat eassumption).
+Qed.
